35862d607c00fc64b238551d30b37b9c40e5a580
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-10 11:41:00 +0300

    complete test on anonymous functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity ='roller-skate' ){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function  mondayWork (task = 'go to the office'){
+    return `This Monday, I will ${task}.`
+}
+
+function  wrapAdjective (wrap = '*'){
+    return function innerFunction (adjective = 'a hard worker'){
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+
+}
+console.log(wrapAdjective()())
